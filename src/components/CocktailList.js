586dcba121a4ebee14ba070ec7de01c99542cbd1
Youssef Zakaria <youssefzakaria10@gmail.com>
2021-03-26 23:56:03 -0400

    Fixing CocktailList component

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -15,7 +15,7 @@ const CocktailList = () => {
   }
   return (
     <section className="section">
-      <h2 className="section-title">cocktails</h2>
+      <h2 className="section-title">cocktails DB</h2>
       <div className="cocktails-center">
         {cocktails.map((item) => {
           return <Cocktail key={item.id} {...item}></Cocktail>;
